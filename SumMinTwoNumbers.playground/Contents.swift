1e6c7c126c6cf8667fda66500c67f5984d00b45a
Furkan Vural <furkanvural52@gmail.com>
2023-02-22 19:16:38 +0300

    Added gitignore file and question solution with best practice

--- SumMinTwoNumbers.playground/Contents.swift
+++ SumMinTwoNumbers.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
+func sumOfTwoSmallestIntegersIn(_ array: [Int]) -> Int {
+  
+  var sortedArray = array.sorted()
+  
+  for i in array {
+    
+    if i < 0{
+        sortedArray.removeFirst()
+    }
+  }
+  
+  let result = sortedArray[0] + sortedArray[1]
+  
+  return result
+}
 
+sumOfTwoSmallestIntegersIn([5, 8, 12, 18, 22])
+
+// Best Practice
+func bestPracticeSumOfTwoSmallestIntegersIn(_ array: [Int]) -> Int {
+  return array.sorted()[0...1].reduce(0, +)
+}
